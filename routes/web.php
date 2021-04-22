a60479f95842d1dcd913f1c36d07595b41e0fa0b
Yurii Kudaltsev <kudaltsev@gmail.com>
2021-04-22 19:38:35 +0300

    added page:index, product, categories

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/categories', function () {
+    return view('categories');
+});
+
+Route::get('/mobiles/iphone_x_64', function () {
+    return view('product');
 });
