a8069db2b0a918cc101d2a1c88c185d6f09226c1
Yurii Kudaltsev <kudaltsev@gmail.com>
2021-04-22 20:16:52 +0300

    change routes and controller

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/categories', function () {
-    return view('categories');
-});
-
-Route::get('/mobiles/iphone_x_64', function () {
-    return view('product');
-});
+Route::get('/', 'App\Http\Controllers\MainController@index');
+Route::get('/categories', 'App\Http\Controllers\MainController@categories');
+Route::get('/mobiles/iphone_x_64', 'App\Http\Controllers\MainController@product');
